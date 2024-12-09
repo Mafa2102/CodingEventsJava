c8c06b0ccf72eab2dcf1cf6a3ece4680642cd753
Maria Usina <maryusina@Marias-Air.attlocal.net>
2024-12-09 12:59:31 -0600

    Models

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -1,5 +1,7 @@
 package org.launchcode.codingevents.controllers;
 
+import org.launchcode.codingevents.data.EventData;
+import org.launchcode.codingevents.models.Event;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,31 +20,66 @@ import java.util.List;
 @RequestMapping("events")
 public class EventController {
 
-
-    private static List<String> events = new ArrayList<>();
+    //private static final HashMap<String, String> events = new HashMap<>();
+    //private static List<Event> events = new ArrayList<>();
 
     @GetMapping
     public String displayAllEvents(Model model) {
-        HashMap<String, String> events = new HashMap<>();
-
-        events.put("Menteaship","A fun meetup for connecting with mentors");
-        events.put("Code With Pride","A fun meetup sponsored by LaunchCode");
-        events.put("Javascripty", "An imaginary meetup for Javascript developers");
-
-        model.addAttribute("events", events);
+        model.addAttribute("title", "All Events");
+        model.addAttribute("events", EventData.getAll());
         return "events/index";
     }
 
-    //lives at /events/create
+//    //lives at /events/create
+//    @GetMapping("create")
+//    public String renderCreateEventForm() {
+//        return "events/create";
+//    }
+
     @GetMapping("create")
-    public String renderCreateEventForm() {
+    public String displayCreateEventForm(Model model) {
+        model.addAttribute("title", "Create Event");
         return "events/create";
     }
 
-    //lives at /events/create
+//    //lives at /events/create
+//    @PostMapping("create")
+//    public String createEvent(@RequestParam String eventName){
+//        events.add(eventName);
+//        return "redirect:/events";
+//    }
+
+    //lives at localhost:8080/events/create but for post
+    //this part for HashMap
+//    @PostMapping("create")
+//    public String processCreateEventForm(@RequestParam String eventName){
+//        events.put("Menteaship","A fun meetup for connecting with mentors");
+//        events.put("Code With Pride","A fun meetup sponsored by LaunchCode");
+//        events.put("Javascripty", "An imaginary meetup for Javascript developers");
+//        return "redirect:/events";
+//    }
+
     @PostMapping("create")
-    public String createEvent(@RequestParam String eventName){
-        events.add(eventName);
+    public String processCreateEventForm(@RequestParam String eventName, @RequestParam String eventDescription) {
+        EventData.add(new Event(eventName, eventDescription));
+        return "redirect:/events";
+    }
+
+    @GetMapping("delete")
+    public String displayDeleteEventForm(Model model){
+        model.addAttribute("title", "Delete Events");
+        model.addAttribute("events", EventData.getAll());
+        return "events/delete";
+    }
+
+    @PostMapping("delete")
+    public String processDeleteEventsForm(@RequestParam(required = false) int[] eventIds) {
+
+        if (eventIds != null) {
+            for (int id : eventIds) {
+                EventData.remove(id);
+            }
+        }
         return "redirect:/events";
     }
 
