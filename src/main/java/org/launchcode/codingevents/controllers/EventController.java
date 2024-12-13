343dca4e5f1a53b567da34dc34a74e5a86e7286e
Maria Usina <maryusina@Marias-Air.attlocal.net>
2024-12-13 17:33:19 -0600

    Displaying errors

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -38,6 +38,7 @@ public class EventController {
     @GetMapping("create")
     public String displayCreateEventForm(Model model) {
         model.addAttribute("title", "Create Event");
+        model.addAttribute(new Event());
         return "events/create";
     }
 
@@ -65,7 +66,7 @@ public class EventController {
 
         if(errors.hasErrors()){
             model.addAttribute("title", "Create Event");
-            model.addAttribute("errorMsg", "Bad data");
+            //model.addAttribute("errorMsg", "Bad data");
             return "events/create";
         }
 
